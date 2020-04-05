6be7f3dfc8de19f89fa268d510c48201d8672bc3
Lutielle <31089973+Lutielle@users.noreply.github.com>
2020-04-05 17:00:16 -0400

    Update wip2.py

--- wip2.py
+++ wip2.py
@@ -1,7 +1,25 @@
-define read_data_tsv(filename):
-    pass
-define read_label_tsv(filename):
-    pass
+def read_data_tsv(filename):
+    data_list = []
+    with open(filename) as cur_file:
+        for line in cur_file:
+            split_data = line.split("\t")
+            for i in range(2, len(split_data)):
+                split_data[i] = int(split_data[i])
+            data_list.append(split_data)
+            
+    cur_file.close()
+    return data_list      
+            
+def read_label_tsv(filename):
+    label_list = []
+    with open(filename) as cur_file:
+        for line in cur_file:
+            split_labels = line.split("\t")
+            split_labels[0] = int(split_labels[0])
+            label_list.append(split_labels)
 
-define main():
+    cur_file.close()
+    return label_list
+
+def main():
     pass
